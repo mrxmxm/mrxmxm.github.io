f49f0c6596c163e695985c6d9d65da735403ac74
tianzhijiaozi123 <2543282869@qq.com>
2024-03-03 22:55:44 +0800

    Site updated: 2024-03-03 22:55:43

---
+++ js/weather.js
@@ -0,0 +1,22 @@
+WIDGET = {
+	"CONFIG": {
+		"modules": "01234",
+		"background": "5",
+		"tmpColor": "FF9900",
+		"tmpSize": "16",
+		"cityColor": "FFFFFF",
+		"citySize": "16",
+		"aqiColor": "FFFFFF",
+		"aqiSize": "16",
+		"weatherIconSize": "24",
+		"alertIconSize": "18",
+		"padding": "10px 10px 10px 10px",
+		"shadow": "0",
+		"language": "auto",
+		"borderRadius": "5",
+		"fixed": "false",
+		"vertical": "center",
+		"horizontal": "center",
+		"key": "4a53cc136cb540c48bac0476e8df2d4f"
+	}
+}
